6e95075f474a0009af6b402af815699e33efc407
HwbDev101 <15012687292@163.com>
2022-02-08 20:41:34 +0800

    fix:修复屏幕旋转数据丢失问题

--- app/src/main/java/com/hwb/wanandroiddemo101/MainActivity.kt
+++ app/src/main/java/com/hwb/wanandroiddemo101/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hwb.wanandroiddemo101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        number = savedInstanceState?.getInt("Number")?:0
         textView.text = "$number"
         buttonAdd.setOnClickListener { textView.text = "${++number}" }
         buttonSub.setOnClickListener { textView.text = "${--number}" }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("Number",number)
     }
 }
