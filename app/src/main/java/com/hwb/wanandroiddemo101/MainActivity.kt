ecfe8588e3c5d7913677447b0cb38f0e2d21ad99
HwbDev101 <15012687292@163.com>
2022-02-08 20:50:15 +0800

    add: add menu

--- app/src/main/java/com/hwb/wanandroiddemo101/MainActivity.kt
+++ app/src/main/java/com/hwb/wanandroiddemo101/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hwb.wanandroiddemo101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,19 @@ class MainActivity : AppCompatActivity() {
         textView.text = "$number"
         buttonAdd.setOnClickListener { textView.text = "${++number}" }
         buttonSub.setOnClickListener { textView.text = "${--number}" }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId ==R.id.menuReset){
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+
     }
 }
